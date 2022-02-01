a33e39b1d037db114afe63df7615f2ca607e21ea
Ilyas Yasin <ilyasyasin072@gmail.com>
2022-02-01 19:04:50 +0700

    add factory to registry dataseed

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
+         \App\Models\Ticket::factory(20)->create();
+         \App\Models\Orders::factory(1)->create();
+         \App\Models\Payment::factory(1)->create();
     }
 }
